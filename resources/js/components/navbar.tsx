447d4bbf7d771d800231922f6b5ea70b93275480
Julie-2605 <julie.cariou2605@gmail.com>
2025-09-26 15:23:43 +0200

    feat: add tailwind css and update navbar links

--- resources/js/components/navbar.tsx
+++ resources/js/components/navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ThemeToggle from "./theme-toggle";
+import { Link } from "@inertiajs/react";
+import { dragAndDrop, home } from "@/routes";
 
 export default function Navbar() {
 
     return (
-        <nav className="flex flex-col items-end p-4 lg:justify-center lg:p-8">
+        <nav className="flex flex-row gap-10 items-center p-4 justify-end lg:p-8">
+            <Link href={home()} className="">🏠 Welcome</Link>
+            <Link href={dragAndDrop()} className="">🎈 Drag & Drop</Link>
             <ThemeToggle />
         </nav>
     );
--- resources/js/pages/drag-and-drop.tsx
+++ resources/js/pages/drag-and-drop.tsx
@@ -14,8 +14,14 @@ export default function DragDropPage() {
                 <h1 className="text-3xl sm:text-4xl font-bold mb-6">Drag & Drop</h1>
 
                 <section className="w-full max-w-md sm:max-w-lg">
+                    <h2 className="text-center text-2xl font-bold mb-3">Top Random</h2>
                     <DragDropList things={things} setThings={setThings} updateOrder={updateOrder} />
                     <button className="mt-6 w-full px-4 py-3 bg-purple-500 hover:bg-purple-700 text-white cursor-pointer font-semibold rounded-lg shadow-md transition-colors durantion-200" onClick={shuffleThings}>Shuffle</button>
+                </section>
+
+                <section className="flex flex-col items-start justify-start w-full mt-10">
+                    <h2 className="text-2xl font-bold mb-3">Card to Order</h2>
+                    {/* Ajout des cards à trier */}
                 </section>
             </main>
         </>
